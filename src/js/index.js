653244aebf9cae12a1798680eeb2b35898018d23
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-02-13 15:15:53 -0400

    Enable phaser on teh project

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+const pixi = require('pixi');
+const p2 = require('p2');
+const Phaser = require('phaser');
+
+let game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+    preload: preload,
+    create: create,
+    update: update
+});
+
+function preload(){}
+function create(){}
+function update(){}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const phaserModulePath = path.join(__dirname, '/node_modules/phaser/');
 
 module.exports = {
     entry: './src/js/index.js',
@@ -14,7 +15,10 @@ module.exports = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/
-            }
+            },
+            { test: /pixi\.js/, loader: 'expose-loader?PIXI' },
+            { test: /phaser-split\.js$/, loader: 'expose-loader?Phaser' },
+            { test: /p2\.js/, loader: 'expose-loader?p2' },
         ]
     },
     plugins: [new HtmlWebpackPlugin({
@@ -23,5 +27,13 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, "build"),
         port: 9000
+    },
+    resolve: {
+        alias: {
+            'phaser': path.join(phaserModulePath, 'build/custom/phaser-split.js'),
+            'pixi': path.join(phaserModulePath, 'build/custom/pixi.js'),
+            'p2': path.join(phaserModulePath, 'build/custom/p2.js')
+
+        }
     }
 };
