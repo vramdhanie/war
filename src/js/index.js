d22712a8e6b089954770a9834cf4c0735e486093
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-02-14 13:51:27 -0400

    Add text display example

--- src/js/index.js
+++ src/js/index.js
@@ -9,5 +9,14 @@ let game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 });
 
 function preload(){}
-function create(){}
+function create(){
+    var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+
+    //  The Text is positioned at 0, 100
+    var text = game.add.text(0, 0, "WAR", style);
+    text.setShadow(3, 3, 'rgba(0.9,0.9,0.9,0.5)', 2);
+
+    text.setTextBounds(0, 100, 800, 100);
+
+}
 function update(){}
